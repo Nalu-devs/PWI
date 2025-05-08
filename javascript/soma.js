b7d5794a468366c8e6a07705446d9917c9e8a96f
Ana Lúcia <pordosol699@gmail.com>
2025-05-08 12:49:30 -0300

    Update and rename soma.html to soma.js
    Mudando o arquivo de html para JS

---
+++ javascript/soma.js
@@ -0,0 +1,4 @@
+var num1 = parseInt(prompt("Digite um numero"))/*O parseInt serve para transformar o que é texto em numero inteiro*/
+var num2 = parseInt(prompt("Digite outro numero"))
+var soma = num1+num2
+alert(soma)
