1c097df9bf048819e9b93734031c97c865e65216
Ana Lúcia <pordosol699@gmail.com>
2025-05-08 12:45:01 -0300

    area.js
    Mudando para arquivo JS

---
+++ javascript/area.js
@@ -0,0 +1,10 @@
+var altura = 20
+var largura = 10
+var area = altura*largura
+alert(area)
+if (altura>largura){
+    alert("Altura é maior que largura")
+}
+else{
+    alert("Altura é menor que largura")
+}
